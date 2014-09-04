669f788a9ce25bed5fdc67d1346cab54d3a64276
Zoe Bryant <zoebryant@Zoes-MacBook-Pro.local>
2014-09-04 17:10:38 +0100

    Zoe & Charlie | Data Handling: Getting User Steps done

--- lib/data_handling/formatted_data.rb
+++ lib/data_handling/formatted_data.rb
@@ -1,15 +1,55 @@
 class FormattedData
 
-	def initialize(json)
-		@json = json
+	def initialize(raw_data)
+		@raw_data = raw_data
+		@steps = []
 	end
 
-	def json
-		@json
+	def raw_data
+		@raw_data
 	end
 
 	def steps
-		[]
+		@steps
 	end
+
+	def get_steps
+		from_each_segment
+	end
+
+	def from_each_segment
+		segments = @raw_data[0]['segments']
+
+		segments.each_with_index do |val, segment_index|
+
+			activities = segments[segment_index]['activities']
+
+			check_for(activities)
+
+		end
+	end
+
+	def check_for(activities)
+		if activities
+			for_all(activities)
+		end
+	end
+
+	def for_all(activities)
+		activities.each_with_index do |val, activity_index|
+
+			track_point = activities[activity_index]['trackPoints']
+
+			for_each(track_point)
+		end
+	end
+
+	def for_each(track_point)
+		track_point.each do |val|
+			@steps <<  track_point
+		end
+	end
+
+
 
 end
