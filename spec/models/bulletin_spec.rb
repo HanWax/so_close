4f467563428e12da9ead8971038a5b28c03ce4ce
Thomas Beeley <tbeeley@gmail.com>
2014-09-10 16:03:35 +0100

    bulletin_feature debugged

--- spec/models/bulletin_spec.rb
+++ spec/models/bulletin_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 require 'Bulletin'
+require 'User'
+require 'Miss'
 
 describe 'bulletins' do
 
-	let(:bulletin)  			{ Bulletin.new("Charles", "Eddie", 18.02, 199) }
+	let(:user1) { User.create(name: 'Charles') }
+	let(:user2) { User.create(name: 'Eddie') }
+	# let(:user1)					{ double: :user, name: 'Eddie', id: 1}
+	# let(:user2)					{ double: :user, name: 'Charles', id: 2}
+	# let(:miss)					{ Miss.create(user_id: 1, neighbour_id: 2, distance: 199, time: 19)}
+
+	let(:miss)					{ Miss.new(distance: 0.11, time: Time.new(2008,6,21, 13,30,0, "+18:02"), user_id: user1.id, neighbour_id: user2.id)}
+	let(:bulletin)  			{ Bulletin.new(miss)}
 
 	context 'when created' do
 
@@ -17,11 +26,11 @@ describe 'bulletins' do
 		end
 
 		it 'should have a time' do
-			expect(bulletin.time).to eq(18.02)
+			expect(bulletin.time).to eq(19)
 		end
 
 		it 'should have a distance' do
-			expect(bulletin.distance).to eq(199)
+			expect(bulletin.distance).to eq(0.11)
 		end
 
 	end
@@ -29,7 +38,7 @@ describe 'bulletins' do
 	context 'conversion methods' do
 
 		it 'should convert the time into a string' do
-			time = 18.02
+			time = 19
 			expect(bulletin.time_converter(time)).to eq 'After the working day was done'
 		end
 
@@ -43,7 +52,7 @@ describe 'bulletins' do
 	context 'displaying bulletin messages' do
 
 		it 'should display the compiled bulletin message' do
-			expect(bulletin.message_randomizer(9)).to eq "After the working day was done, Eddie looked set to bump into Charles, but with the distance between them at but a longbow's range, a twist of fate pulled them in different directions. Eddie must act quickly, Charles is scheduled to board the ferry back to Biarritz any day now..."
+			expect(bulletin.message_randomizer(9)).to eq "After the working day was done, Eddie looked set to bump into Charles, but with the distance between them at but a football pitch, a twist of fate pulled them in different directions. Eddie must act quickly, Charles is scheduled to board the ferry back to Biarritz any day now..."
 		end
 	end
 
